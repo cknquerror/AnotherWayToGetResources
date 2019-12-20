ec5db91d2e20aa8ec27ec8b1f28caaa092a87a49
Andrey Shapovalov <pinkoty04@gmail.com>
2019-12-21 01:38:56 +0300

    proxy fix

--- src/main/java/cknquerror/awtgr/proxy/CommonProxy.java
+++ src/main/java/cknquerror/awtgr/proxy/CommonProxy.java
@@ -1,4 +1,4 @@
-package ru.cknquerror.awtgr.proxy;
+package cknquerror.awtgr.proxy;
 
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
